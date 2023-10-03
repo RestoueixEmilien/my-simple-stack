46e6302e0fe15e6c52fc645f8ba87f1f170da9f1
RestoueixEmilien <emilien.restoueix@iut-rodez.fr>
2023-10-03 17:34:06 +0200

    1er commit

---
+++ src/main/java/but3/eres/App.java
@@ -0,0 +1,13 @@
+package but3.eres;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/but3/eres/AppTest.java
@@ -0,0 +1,16 @@
+package but3.eres;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() { assertTrue(true); }
+}
